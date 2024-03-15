c17182cc840c0bb2008a699a5f42006241c8e25e
francescodalmonte <dalmonte.fr@gmail.com>
2024-03-15 15:50:11 +0100

    final version of Bscan correction

--- plots_utils.py
+++ plots_utils.py
@@ -67,14 +67,86 @@ def plot_Bscan(data: np.ndarray,
         
         th_data = padded_th_data
 
-    ax.imshow(th_data, aspect='auto', **kwargs)
-    #ax.set_ylabel("Time/depth (au)")
-    ax.set_yticks([])
+    ax.imshow(th_data, aspect='equal', **kwargs)
+    ax.set_ylabel("Time/depth (au)")
     ax.set_xlabel("Element")
     ax.set_title(title)
 
     return th_data
     
+
+def plot_Bscan_wcorrection(data: np.ndarray, 
+                           idx: int,
+                           ax: matplotlib.axes.Axes,
+                           angle: int,
+                           res_depth: float,
+                           pitch: float,
+                           title: str = "B-Scan",
+                           th: float = 0., 
+                           **kwargs):
+    """
+
+
+    """
+    th_data = data[:,idx].T.copy()
+    th_data[th_data<th] = np.nan
+
+    if angle != 0:
+
+        # preliminar computation of transformation parameters
+        angle_rad = angle*np.pi/180
+        res_x = pitch*np.cos(angle_rad)
+        res_y = res_depth/np.cos(angle_rad)
+        shearY = pitch*np.sin(angle_rad)/res_y
+        rXY_factor = res_x/res_y
+
+
+        print(f"angle: {angle}, res_x: {res_x}, res_y: {res_y}, shearY: {shearY}")
+
+
+        # SHEAR MAPPING (y axis)
+        rows, cols = th_data.shape
+        # upsample 10x in y direction (enables a more precise shear mapping)
+        th_data = cv.resize(th_data.copy(), dsize = None,
+                            fx=1., fy=10., interpolation=cv.INTER_LINEAR)
+        shearY_r = int(np.round(shearY*10, 0))
+        # preliminar padding 
+        th_data = np.pad(th_data, ((0, shearY_r*(cols-1)), (0, 0)),
+                         mode='constant', constant_values=np.nan)
+        # shear mapping
+        # (each column is manually shifted donwwards by a factor proportional to column index)
+        for col in range(cols):
+            th_data[:, col] = np.roll(th_data[:, col], shearY_r*(col))
+        # downsample back to original resolution
+        th_data = cv.resize(th_data, dsize = None, fx=1., fy=0.1, interpolation=cv.INTER_AREA)
+
+        # UPSAMPLING OF x AXIS (to match y resolution)
+        th_data = cv.resize(th_data, dsize = None, fx=rXY_factor, fy=1., interpolation=cv.INTER_LINEAR)
+
+        # ROTATION
+        M_rotation = cv.getRotationMatrix2D([0,0], angle, scale=1)
+        rows_m, cols_m = th_data.shape
+        out_shape = (int(cols_m*np.cos(angle_rad)+rows_m*(np.sin(angle_rad))),
+                     int(rows_m*np.cos(angle_rad)-cols_m*(np.sin(angle_rad))))
+        th_data = cv.warpAffine(th_data, M_rotation, out_shape,
+                                borderMode=cv.BORDER_CONSTANT, borderValue = np.nan)
+                
+    ax.imshow(th_data, aspect='equal', **kwargs)
+
+    #x_axis_mm = np.arange(0, out_shape[0]*res_x, res_x)
+    #y_axis_mm = np.arange(0, out_shape[1]*res_y, res_y)
+
+    ax.set_xticks(np.linspace(0, out_shape[0], 10),
+                  np.round(np.linspace(0, out_shape[0], 10)*res_y, 1))
+    ax.set_yticks(np.linspace(0, out_shape[1], 6),
+                  np.round(np.linspace(0, out_shape[1], 6)*res_y, 1))
+    ax.set_ylabel("Depth [mm]")
+    ax.set_xlabel("y [mm]")
+    ax.set_title(title)
+
+    return th_data
+
+
 
 def plot_Dscan(data: np.ndarray, 
                idx: int,
@@ -109,6 +181,7 @@ def plot_Cscan(data: np.ndarray,
                ax: matplotlib.axes.Axes,
                title: str = "C-Scan",
                th: float = 0.,
+               pos_mm: np.ndarray = None,
                **kwargs):
     """
     Plot a 2-d C-Scan on an existing axis.
@@ -124,10 +197,16 @@ def plot_Cscan(data: np.ndarray,
     """
     cscan = np.max(data, axis=2)
     cscan[cscan<th]=np.nan
+
     ax.imshow(cscan, aspect='auto', **kwargs)
+    if pos_mm is None:
+        ax.set_xlabel("Position (au)")
+    else:
+        ax.set_xlabel("Position (mm)")
+        ax.set_xticks(np.arange(0, len(pos_mm), int(len(pos_mm)/10)))
+        ax.set_xticklabels(pos_mm[::int(len(pos_mm)/10)])
+
     ax.set_ylabel("Elements")
-    ax.set_xlabel("Position (au)")
     ax.set_title(title)
 
     return cscan
-    
