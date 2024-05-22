44c6781a5efdebf8ae099239b5fa034ea2facc25
francescodalmonte <dalmonte.fr@gmail.com>
2024-05-22 11:39:31 +0200

    removed scripts arg from setup

--- PAUT_preprocessing-pkg/setup.py
+++ PAUT_preprocessing-pkg/setup.py
@@ -4,6 +4,5 @@ setup(name='PAUT_preprocessing-pkg',
       version='1.0',
       author='Francesco Dalmonte',
       packages=['PAUT_preprocessing',
-                'PAUT_preprocessing.test',],
-      scripts=['bin/temp.py']
+                'PAUT_preprocessing.test',]
       )
